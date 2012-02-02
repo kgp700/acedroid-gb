64dd4103f1cb6ef8c4164d38b9a8b40adf0b5971
kgp700 <neokim20@gmail.com>
2012-02-02 19:15:43 +0900

    ADD: Kineto Gand module

---
+++ include/generated/compile.h
@@ -0,0 +1,9 @@
+/* This file is auto generated, version 1 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#1 PREEMPT Thu Feb 2 19:12:52 KST 2012"
+#define LINUX_COMPILE_TIME "19:12:52"
+#define LINUX_COMPILE_BY "root"
+#define LINUX_COMPILE_HOST "KGPNeok-Nexroid-Dev"
+#define LINUX_COMPILE_DOMAIN "(none)"
+#define LINUX_COMPILER "gcc version 4.5.4 20110808 (prerelease) (Linaro GCC 4.5-2011.08) "
---
+++ include/generated/utsrelease.h
@@ -0,0 +1,1 @@
+#define UTS_RELEASE "2.6.35.14-AceDroid-Kernel-V0.2a-CFS-TRB-g0c4d250"
