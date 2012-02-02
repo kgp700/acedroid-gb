cb03ee7116c676091a746a42341123dec2c7bd39
kgp700 <neokim20@gmail.com>
2012-02-02 23:58:25 +0900

    apply git ignore

--- include/generated/compile.h
+++
@@ -1,9 +0,0 @@
-/* This file is auto generated, version 4 */
-/*  PREEMPT */
-#define UTS_MACHINE "arm"
-#define UTS_VERSION "#4 PREEMPT Thu Feb 2 22:44:50 KST 2012"
-#define LINUX_COMPILE_TIME "22:44:50"
-#define LINUX_COMPILE_BY "root"
-#define LINUX_COMPILE_HOST "KGPNeok-Nexroid-Dev"
-#define LINUX_COMPILE_DOMAIN "(none)"
-#define LINUX_COMPILER "gcc version 4.5.4 20110808 (prerelease) (Linaro GCC 4.5-2011.08) "
