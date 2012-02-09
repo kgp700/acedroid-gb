bd59c275b606944d9b3ee1f5c0bb659f1d8207bb
kgp700 <neokim20@gmail.com>
2012-02-09 18:17:46 +0900

    ADD neokdemand2 governor and change settings

--- drivers/cpufreq/cpufreq_neokdemand2.c
+++ drivers/cpufreq/cpufreq_neokdemand2.c
@@ -1,5 +1,5 @@
 /*
- *  drivers/cpufreq/cpufreq_neokdemand.c
+ *  drivers/cpufreq/cpufreq_neokdemand2.c
  *
  *  Copyright (C)  2001 Russell King
  *            (C)  2003 Venkatesh Pallipadi <venkatesh.pallipadi@intel.com>.
@@ -30,9 +30,14 @@
 
 #define DEF_FREQUENCY_DOWN_DIFFERENTIAL		(10)
 #define DEF_FREQUENCY_UP_THRESHOLD		(80)
-#define MICRO_FREQUENCY_DOWN_DIFFERENTIAL	(3)
+#define DEF_SAMPLING_DOWN_FACTOR                (4)
+#define MAX_SAMPLING_DOWN_FACTOR                (100000)
+#define DEF_SAMPLING_DOWN_MAX_MOMENTUM		(18)
+#define DEF_SAMPLING_DOWN_MOMENTUM_SENSITIVITY	(90)
+#define MAX_SAMPLING_DOWN_MOMENTUM_SENSITIVITY	(1000)
+#define MICRO_FREQUENCY_DOWN_DIFFERENTIAL	(8)
 #define MICRO_FREQUENCY_UP_THRESHOLD		(95)
-#define MICRO_FREQUENCY_MIN_SAMPLE_RATE		(95000)
+#define MICRO_FREQUENCY_MIN_SAMPLE_RATE		(97000)
 #define MIN_FREQUENCY_UP_THRESHOLD		(11)
 #define MAX_FREQUENCY_UP_THRESHOLD		(100)
 
@@ -58,11 +63,11 @@ static void do_dbs_timer(struct work_struct *work);
 static int cpufreq_governor_dbs(struct cpufreq_policy *policy,
 				unsigned int event);
 
-#ifndef CONFIG_CPU_FREQ_DEFAULT_GOV_NEOKDEMAND
+#ifndef CONFIG_CPU_FREQ_DEFAULT_GOV_neokdemand2
 static
 #endif
-struct cpufreq_governor cpufreq_gov_neokdemand = {
-       .name                   = "neokdemand",
+struct cpufreq_governor cpufreq_gov_neokdemand2 = {
+       .name                   = "neokdemand2",
        .governor               = cpufreq_governor_dbs,
        .max_transition_latency = TRANSITION_LATENCY_LIMIT,
        .owner                  = THIS_MODULE,
@@ -75,6 +80,7 @@ struct cpu_dbs_info_s {
 	cputime64_t prev_cpu_idle;
 	cputime64_t prev_cpu_iowait;
 	cputime64_t prev_cpu_wall;
+	/*unsigned int prev_cpu_wall_delta;*/
 	cputime64_t prev_cpu_nice;
 	struct cpufreq_policy *cur_policy;
 	struct delayed_work work;
@@ -82,6 +88,8 @@ struct cpu_dbs_info_s {
 	unsigned int freq_lo;
 	unsigned int freq_lo_jiffies;
 	unsigned int freq_hi_jiffies;
+	unsigned int rate_mult;
+	unsigned int momentum_adder;
 	int cpu;
 	unsigned int sample_type:1;
 	/*
@@ -101,20 +109,28 @@ static unsigned int dbs_enable;	/* number of CPUs using this policy */
  */
 static DEFINE_MUTEX(dbs_mutex);
 
-static struct workqueue_struct	*kneokdemand_wq;
+static struct workqueue_struct	*kneokdemand2_wq;
 
 static struct dbs_tuners {
 	unsigned int sampling_rate;
 	unsigned int up_threshold;
 	unsigned int down_differential;
 	unsigned int ignore_nice;
+	unsigned int sampling_down_factor;
+	unsigned int sampling_down_momentum;
+	unsigned int sampling_down_max_momentum;
+	unsigned int sampling_down_momentum_sensitivity;
 	unsigned int powersave_bias;
 	unsigned int io_is_busy;
 } dbs_tuners_ins = {
 	.up_threshold = DEF_FREQUENCY_UP_THRESHOLD,
+	.sampling_down_factor = DEF_SAMPLING_DOWN_FACTOR,
+	.sampling_down_momentum = 0,
+	.sampling_down_max_momentum = DEF_SAMPLING_DOWN_MAX_MOMENTUM,
+	.sampling_down_momentum_sensitivity = DEF_SAMPLING_DOWN_MOMENTUM_SENSITIVITY,
 	.down_differential = DEF_FREQUENCY_DOWN_DIFFERENTIAL,
 	.ignore_nice = 0,
-	.powersave_bias = 35,
+	.powersave_bias = 37,
 };
 
 static inline cputime64_t get_cpu_idle_time_jiffy(unsigned int cpu,
@@ -215,18 +231,18 @@ static unsigned int powersave_bias_target(struct cpufreq_policy *policy,
 	return freq_hi;
 }
 
-static void neokdemand_powersave_bias_init_cpu(int cpu)
+static void neokdemand2_powersave_bias_init_cpu(int cpu)
 {
 	struct cpu_dbs_info_s *dbs_info = &per_cpu(od_cpu_dbs_info, cpu);
 	dbs_info->freq_table = cpufreq_frequency_get_table(cpu);
 	dbs_info->freq_lo = 0;
 }
 
-static void neokdemand_powersave_bias_init(void)
+static void neokdemand2_powersave_bias_init(void)
 {
 	int i;
 	for_each_online_cpu(i) {
-		neokdemand_powersave_bias_init_cpu(i);
+		neokdemand2_powersave_bias_init_cpu(i);
 	}
 }
 
@@ -235,7 +251,7 @@ static void neokdemand_powersave_bias_init(void)
 static ssize_t show_sampling_rate_max(struct kobject *kobj,
 				      struct attribute *attr, char *buf)
 {
-	printk_once(KERN_INFO "CPUFREQ: neokdemand sampling_rate_max "
+	printk_once(KERN_INFO "CPUFREQ: neokdemand2 sampling_rate_max "
 	       "sysfs file is deprecated - used by: %s\n", current->comm);
 	return sprintf(buf, "%u\n", -1U);
 }
@@ -249,7 +265,7 @@ static ssize_t show_sampling_rate_min(struct kobject *kobj,
 define_one_global_ro(sampling_rate_max);
 define_one_global_ro(sampling_rate_min);
 
-/* cpufreq_neokdemand Governor Tunables */
+/* cpufreq_neokdemand2 Governor Tunables */
 #define show_one(file_name, object)					\
 static ssize_t show_##file_name						\
 (struct kobject *kobj, struct attribute *attr, char *buf)              \
@@ -260,20 +276,23 @@ show_one(sampling_rate, sampling_rate);
 show_one(io_is_busy, io_is_busy);
 show_one(up_threshold, up_threshold);
 show_one(down_differential, down_differential);
+show_one(sampling_down_factor, sampling_down_factor);
+show_one(sampling_down_max_momentum, sampling_down_max_momentum);
+show_one(sampling_down_momentum_sensitivity, sampling_down_momentum_sensitivity);
 show_one(ignore_nice_load, ignore_nice);
 show_one(powersave_bias, powersave_bias);
 
 /*** delete after deprecation time ***/
 
 #define DEPRECATION_MSG(file_name)					\
-	printk_once(KERN_INFO "CPUFREQ: Per core neokdemand sysfs "	\
+	printk_once(KERN_INFO "CPUFREQ: Per core neokdemand2 sysfs "	\
 		    "interface is deprecated - " #file_name "\n");
 
 #define show_one_old(file_name)						\
 static ssize_t show_##file_name##_old					\
 (struct cpufreq_policy *unused, char *buf)				\
 {									\
-	printk_once(KERN_INFO "CPUFREQ: Per core neokdemand sysfs "	\
+	printk_once(KERN_INFO "CPUFREQ: Per core neokdemand2 sysfs "	\
 		    "interface is deprecated - " #file_name "\n");	\
 	return show_##file_name(NULL, NULL, buf);			\
 }
@@ -341,6 +360,75 @@ static ssize_t store_up_threshold(struct kobject *a, struct attribute *b,
 	return count;
 }
 
+static ssize_t store_sampling_down_factor(struct kobject *a,
+                       struct attribute *b, const char *buf, size_t count)
+{
+	unsigned int input, j;
+	int ret;
+	ret = sscanf(buf, "%u", &input);
+
+	if (ret != 1 || input > MAX_SAMPLING_DOWN_FACTOR || input < 1)
+		return -EINVAL;
+	mutex_lock(&dbs_mutex);
+	dbs_tuners_ins.sampling_down_factor = input;
+
+	/* Reset down sampling multiplier in case it was active */
+	for_each_online_cpu(j) {
+		struct cpu_dbs_info_s *dbs_info;
+		dbs_info = &per_cpu(od_cpu_dbs_info, j);
+		dbs_info->rate_mult = 1;
+	}
+	mutex_unlock(&dbs_mutex);
+
+	return count;
+}
+
+static ssize_t store_sampling_down_max_momentum(struct kobject *a,
+                       struct attribute *b, const char *buf, size_t count)
+{
+	unsigned int input, j;
+	int ret;
+	ret = sscanf(buf, "%u", &input);
+
+	if (ret != 1 || input > MAX_SAMPLING_DOWN_FACTOR - dbs_tuners_ins.sampling_down_factor  || input < 0)
+		return -EINVAL;
+	mutex_lock(&dbs_mutex);
+	dbs_tuners_ins.sampling_down_max_momentum = input;
+
+	/* Reset momentum_adder*/
+	for_each_online_cpu(j) {
+		struct cpu_dbs_info_s *dbs_info;
+		dbs_info = &per_cpu(od_cpu_dbs_info, j);
+		dbs_info->momentum_adder = 0;
+	}
+	mutex_unlock(&dbs_mutex);
+
+	return count;
+}
+
+static ssize_t store_sampling_down_momentum_sensitivity(struct kobject *a,
+                       struct attribute *b, const char *buf, size_t count)
+{
+	unsigned int input, j;
+	int ret;
+	ret = sscanf(buf, "%u", &input);
+
+	if (ret != 1 || input > MAX_SAMPLING_DOWN_MOMENTUM_SENSITIVITY  || input < 1)
+		return -EINVAL;
+	mutex_lock(&dbs_mutex);
+	dbs_tuners_ins.sampling_down_momentum_sensitivity = input;
+
+	/* Reset momentum_adder*/
+	for_each_online_cpu(j) {
+		struct cpu_dbs_info_s *dbs_info;
+		dbs_info = &per_cpu(od_cpu_dbs_info, j);
+		dbs_info->momentum_adder = 0;
+	}
+	mutex_unlock(&dbs_mutex);
+
+	return count;
+}
+
 static ssize_t store_ignore_nice_load(struct kobject *a, struct attribute *b,
 				      const char *buf, size_t count)
 {
@@ -393,39 +481,43 @@ static ssize_t store_powersave_bias(struct kobject *a, struct attribute *b,
 
 	mutex_lock(&dbs_mutex);
 	dbs_tuners_ins.powersave_bias = input;
-	neokdemand_powersave_bias_init();
+	neokdemand2_powersave_bias_init();
 	mutex_unlock(&dbs_mutex);
 
 	return count;
 }
 
-static ssize_t store_down_differential(struct kobject *a, struct attribute *b,
-				    const char *buf, size_t count)
-{
-	unsigned int input;
-	int ret;
-	ret = sscanf(buf, "%u", &input);
+static ssize_t store_down_differential(struct kobject *a, struct attribute *b, 
+                                   const char *buf, size_t count) 
+{ 
+       unsigned int input; 
+       int ret; 
+       ret = sscanf(buf, "%u", &input); 
+ 
+       if (ret != 1) 
+               return -EINVAL; 
+ 
+       if (input > 30) 
+               input = 30; 
+ 
+       if (input < 0) 
+               input = 0; 
+ 
+       mutex_lock(&dbs_mutex); 
+       dbs_tuners_ins.down_differential = input; 
+       mutex_unlock(&dbs_mutex); 
+ 
+       return count; 
+} 
 
-	if (ret != 1)
-		return -EINVAL;
-
-	if (input > 30)
-		input = 30;
-
-	if (input < 0)
-		input = 0;
-
-	mutex_lock(&dbs_mutex);
-	dbs_tuners_ins.down_differential = input;
-	mutex_unlock(&dbs_mutex);
-
-	return count;
-}
 
 define_one_global_rw(sampling_rate);
 define_one_global_rw(io_is_busy);
 define_one_global_rw(up_threshold);
 define_one_global_rw(down_differential);
+define_one_global_rw(sampling_down_factor);
+define_one_global_rw(sampling_down_max_momentum);
+define_one_global_rw(sampling_down_momentum_sensitivity);
 define_one_global_rw(ignore_nice_load);
 define_one_global_rw(powersave_bias);
 
@@ -435,6 +527,9 @@ static struct attribute *dbs_attributes[] = {
 	&sampling_rate.attr,
 	&up_threshold.attr,
 	&down_differential.attr,
+	&sampling_down_factor.attr,
+	&sampling_down_max_momentum.attr,
+	&sampling_down_momentum_sensitivity.attr,
 	&ignore_nice_load.attr,
 	&powersave_bias.attr,
 	&io_is_busy.attr,
@@ -443,7 +538,7 @@ static struct attribute *dbs_attributes[] = {
 
 static struct attribute_group dbs_attr_group = {
 	.attrs = dbs_attributes,
-	.name = "neokdemand",
+	.name = "neokdemand2",
 };
 
 /*** delete after deprecation time ***/
@@ -452,7 +547,7 @@ static struct attribute_group dbs_attr_group = {
 static ssize_t store_##file_name##_old					\
 (struct cpufreq_policy *unused, const char *buf, size_t count)		\
 {									\
-       printk_once(KERN_INFO "CPUFREQ: Per core neokdemand sysfs "	\
+       printk_once(KERN_INFO "CPUFREQ: Per core neokdemand2 sysfs "	\
 		   "interface is deprecated - " #file_name "\n");	\
        return store_##file_name(NULL, NULL, buf, count);		\
 }
@@ -478,7 +573,7 @@ static struct attribute *dbs_attributes_old[] = {
 
 static struct attribute_group dbs_attr_group_old = {
        .attrs = dbs_attributes_old,
-       .name = "neokdemand",
+       .name = "neokdemand2",
 };
 
 /*** delete after deprecation time ***/
@@ -527,6 +622,10 @@ static void dbs_check_cpu(struct cpu_dbs_info_s *this_dbs_info)
 		unsigned int idle_time, wall_time, iowait_time;
 		unsigned int load, load_freq;
 		int freq_avg;
+		/*bool deep_sleep_detected = false;*/
+		/* the evil magic numbers, only 2 at least */
+		/*const unsigned int deep_sleep_backoff = 10;
+		const unsigned int deep_sleep_factor = 5;*/
 
 		j_dbs_info = &per_cpu(od_cpu_dbs_info, j);
 
@@ -537,6 +636,32 @@ static void dbs_check_cpu(struct cpu_dbs_info_s *this_dbs_info)
 				j_dbs_info->prev_cpu_wall);
 		j_dbs_info->prev_cpu_wall = cur_wall_time;
 
+		/*
+		 * Ignore wall delta jitters in both directions.  An
+		 * exceptionally long wall_time will likely result
+		 * idle but it was waken up to do work so the next
+		 * slice is less likely to want to run at low
+		 * frequency. Let's evaluate the next slice instead of
+		 * the idle long one that passed already and it's too
+		 * late to reduce in frequency.  As opposed an
+		 * exceptionally short slice that just run at low
+		 * frequency is unlikely to be idle, but we may go
+		 * back to idle pretty soon and that not idle slice
+		 * already passed. If short slices will keep coming
+		 * after a series of long slices the exponential
+		 * backoff will converge faster and we'll react faster
+		 * to high load. As opposed we'll decay slower
+		 * towards low load and long idle times.
+		 */
+		/*if (j_dbs_info->prev_cpu_wall_delta >
+		  wall_time * deep_sleep_factor ||
+		    j_dbs_info->prev_cpu_wall_delta * deep_sleep_factor <
+		    wall_time)
+			deep_sleep_detected = true;
+		j_dbs_info->prev_cpu_wall_delta =
+			(j_dbs_info->prev_cpu_wall_delta * deep_sleep_backoff
+			 + wall_time) / (deep_sleep_backoff+1);
+*/
 		idle_time = (unsigned int) cputime64_sub(cur_idle_time,
 				j_dbs_info->prev_cpu_idle);
 		j_dbs_info->prev_cpu_idle = cur_idle_time;
@@ -562,8 +687,11 @@ static void dbs_check_cpu(struct cpu_dbs_info_s *this_dbs_info)
 			idle_time += jiffies_to_usecs(cur_nice_jiffies);
 		}
 
+		/*if (deep_sleep_detected)
+			continue; */
+
 		/*
-		 * For the purpose of neokdemand, waiting for disk IO is an
+		 * For the purpose of neokdemand2, waiting for disk IO is an
 		 * indication that you're performance critical, and not that
 		 * the system is actually idle. So subtract the iowait time
 		 * from the cpu idle time.
@@ -588,9 +716,35 @@ static void dbs_check_cpu(struct cpu_dbs_info_s *this_dbs_info)
 
 	/* Check for frequency increase */
 	if (max_load_freq > dbs_tuners_ins.up_threshold * policy->cur) {
+		/* If switching to max speed, apply sampling_down_factor */
+		if (policy->cur < policy->max)
+			this_dbs_info->rate_mult =
+				dbs_tuners_ins.sampling_down_factor;
 		dbs_freq_increase(policy, policy->max);
+		
+		/* Calculate momentum and update sampling down factor */
+		if (++this_dbs_info->momentum_adder > dbs_tuners_ins.sampling_down_momentum_sensitivity)
+			this_dbs_info->momentum_adder = dbs_tuners_ins.sampling_down_momentum_sensitivity;
+		
+		dbs_tuners_ins.sampling_down_momentum = (this_dbs_info->momentum_adder * dbs_tuners_ins.sampling_down_max_momentum) 
+							  / dbs_tuners_ins.sampling_down_momentum_sensitivity;
+		
+		if (dbs_tuners_ins.sampling_down_momentum > dbs_tuners_ins.sampling_down_max_momentum)
+			dbs_tuners_ins.sampling_down_momentum = dbs_tuners_ins.sampling_down_max_momentum;
+		
+		dbs_tuners_ins.sampling_down_factor = DEF_SAMPLING_DOWN_FACTOR + dbs_tuners_ins.sampling_down_momentum;
+		
 		return;
 	}
+
+	/* Calculate momentum and update sampling down factor */
+	if (this_dbs_info->momentum_adder > 0)
+		this_dbs_info->momentum_adder--;
+
+	dbs_tuners_ins.sampling_down_momentum = (this_dbs_info->momentum_adder * dbs_tuners_ins.sampling_down_max_momentum) 
+						  / dbs_tuners_ins.sampling_down_momentum_sensitivity;
+
+	dbs_tuners_ins.sampling_down_factor = DEF_SAMPLING_DOWN_FACTOR + dbs_tuners_ins.sampling_down_momentum;
 
 	/* Check for frequency decrease */
 	/* if we cannot reduce the frequency anymore, break out early */
@@ -609,6 +763,9 @@ static void dbs_check_cpu(struct cpu_dbs_info_s *this_dbs_info)
 		freq_next = max_load_freq /
 				(dbs_tuners_ins.up_threshold -
 				 dbs_tuners_ins.down_differential);
+
+		/* No longer fully busy, reset rate_mult */
+		this_dbs_info->rate_mult = 1;
 
 		if (freq_next < policy->min)
 			freq_next = policy->min;
@@ -633,7 +790,8 @@ static void do_dbs_timer(struct work_struct *work)
 	int sample_type = dbs_info->sample_type;
 
 	/* We want all CPUs to do sampling nearly on same jiffy */
-	int delay = usecs_to_jiffies(dbs_tuners_ins.sampling_rate);
+	int delay = usecs_to_jiffies(dbs_tuners_ins.sampling_rate
+		* dbs_info->rate_mult);
 
 	if (num_online_cpus() > 1)
 		delay -= jiffies % delay;
@@ -654,7 +812,7 @@ static void do_dbs_timer(struct work_struct *work)
 		__cpufreq_driver_target(dbs_info->cur_policy,
 			dbs_info->freq_lo, CPUFREQ_RELATION_H);
 	}
-	queue_delayed_work_on(cpu, kneokdemand_wq, &dbs_info->work, delay);
+	queue_delayed_work_on(cpu, kneokdemand2_wq, &dbs_info->work, delay);
 	mutex_unlock(&dbs_info->timer_mutex);
 }
 
@@ -662,13 +820,11 @@ static inline void dbs_timer_init(struct cpu_dbs_info_s *dbs_info)
 {
 	/* We want all CPUs to do sampling nearly on same jiffy */
 	int delay = usecs_to_jiffies(dbs_tuners_ins.sampling_rate);
-
-	if (num_online_cpus() > 1)
-		delay -= jiffies % delay;
+	delay -= jiffies % delay;
 
 	dbs_info->sample_type = DBS_NORMAL_SAMPLE;
 	INIT_DELAYED_WORK_DEFERRABLE(&dbs_info->work, do_dbs_timer);
-	queue_delayed_work_on(dbs_info->cpu, kneokdemand_wq, &dbs_info->work,
+	queue_delayed_work_on(dbs_info->cpu, kneokdemand2_wq, &dbs_info->work,
 		delay);
 }
 
@@ -697,7 +853,7 @@ static int should_io_be_busy(void)
 	    boot_cpu_data.x86_model >= 15)
 		return 1;
 #endif
-	return 0;
+	return 1;
 }
 
 static int cpufreq_governor_dbs(struct cpufreq_policy *policy,
@@ -737,7 +893,9 @@ static int cpufreq_governor_dbs(struct cpufreq_policy *policy,
 			}
 		}
 		this_dbs_info->cpu = cpu;
-		neokdemand_powersave_bias_init_cpu(cpu);
+		this_dbs_info->rate_mult = 1;
+		this_dbs_info->momentum_adder = 0;
+		neokdemand2_powersave_bias_init_cpu(cpu);
 		/*
 		 * Start the timerschedule work, when this governor
 		 * is used for first time
@@ -824,32 +982,32 @@ static int __init cpufreq_gov_dbs_init(void)
 			MIN_SAMPLING_RATE_RATIO * jiffies_to_usecs(10);
 	}
 
-	kneokdemand_wq = create_workqueue("kneokdemand");
-	if (!kneokdemand_wq) {
-		printk(KERN_ERR "Creation of kneokdemand failed\n");
+	kneokdemand2_wq = create_workqueue("kneokdemand2");
+	if (!kneokdemand2_wq) {
+		printk(KERN_ERR "Creation of kneokdemand2 failed\n");
 		return -EFAULT;
 	}
-	err = cpufreq_register_governor(&cpufreq_gov_neokdemand);
+	err = cpufreq_register_governor(&cpufreq_gov_neokdemand2);
 	if (err)
-		destroy_workqueue(kneokdemand_wq);
+		destroy_workqueue(kneokdemand2_wq);
 
 	return err;
 }
 
 static void __exit cpufreq_gov_dbs_exit(void)
 {
-	cpufreq_unregister_governor(&cpufreq_gov_neokdemand);
-	destroy_workqueue(kneokdemand_wq);
+	cpufreq_unregister_governor(&cpufreq_gov_neokdemand2);
+	destroy_workqueue(kneokdemand2_wq);
 }
 
 
 MODULE_AUTHOR("Venkatesh Pallipadi <venkatesh.pallipadi@intel.com>");
 MODULE_AUTHOR("Alexey Starikovskiy <alexey.y.starikovskiy@intel.com>");
-MODULE_DESCRIPTION("'cpufreq_neokdemand' - A dynamic cpufreq governor for "
+MODULE_DESCRIPTION("'cpufreq_neokdemand2' - A dynamic cpufreq governor for "
 	"Low Latency Frequency Transition capable processors");
 MODULE_LICENSE("GPL");
 
-#ifdef CONFIG_CPU_FREQ_DEFAULT_GOV_NEOKDEMAND
+#ifdef CONFIG_CPU_FREQ_DEFAULT_GOV_neokdemand2
 fs_initcall(cpufreq_gov_dbs_init);
 #else
 module_init(cpufreq_gov_dbs_init);
