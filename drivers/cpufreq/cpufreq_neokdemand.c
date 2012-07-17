c80d842052ecebb20b8f7f10b267f9892aa697fd
kgp700 <neokim20@gmail.com>
2012-07-17 21:00:25 +0900

    Tweak neokdemand

--- drivers/cpufreq/cpufreq_neokdemand.c
+++ drivers/cpufreq/cpufreq_neokdemand.c
@@ -138,13 +138,13 @@ static unsigned int ramp_down_step;
 /*
  * CPU freq will be increased if measured load > max_cpu_load;
  */
-#define DEFAULT_MAX_CPU_LOAD 87
+#define DEFAULT_MAX_CPU_LOAD 85
 static unsigned long max_cpu_load;
 
 /*
  * CPU freq will be decreased if measured load < min_cpu_load;
  */
-#define DEFAULT_MIN_CPU_LOAD 70
+#define DEFAULT_MIN_CPU_LOAD 65
 static unsigned long min_cpu_load;
 
 
